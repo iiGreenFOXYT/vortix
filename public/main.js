05be364484f614d81c7d6350a288215d452b49d7
iiGreenFOXYT <karemyossry147@gmail.com>
2023-08-20 17:14:42 +0300

    orgnize

--- public/main.js
+++ public/main.js
@@ -22,81 +22,10 @@ const theBody = document.querySelector('body');
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  const imageInput = document.getElementById("imageInput");
-  const removeButton = document.getElementById("removeButton");
-  const previewImage = document.getElementById("previewImage");
-  const progressBar = document.getElementById("progressBar");
-  const progressFill = document.getElementById("progressFill");
-  const resultDiv = document.getElementById("result");
-  const removedImage = document.getElementById("removedImage");
-  const downloadButton = document.getElementById("downloadButton");
-
-  removeButton.addEventListener("click", async () => {
-    if (!imageInput.files.length) {
-      alert("Please select an image.");
-      return;
-    }
-
-    const apiKey = "3FYJwQMAdNc1SKoQpHU92iZN"; 
-    const imageFile = imageInput.files[0];
-
-    const formData = new FormData();
-    formData.append("image_file", imageFile);
-
-    try {
-      progressBar.style.display = "block";
-      progressFill.style.width = "0";
-
-      const response = await fetch("https://api.remove.bg/v1.0/removebg", {
-        method: "POST",
-        headers: {
-          "X-Api-Key": apiKey,
-        },
-        body: formData,
-      });
-
-      if (!response.ok) {
-        throw new Error("API request failed");
-      }
-
-      const blob = await response.blob();
-      const imageUrl = URL.createObjectURL(blob);
-
-      removedImage.src = imageUrl;
-      downloadButton.style.display = "block";
-      resultDiv.style.display = "block";
-    } catch (error) {
-      resultDiv.innerHTML = `<p>Error: ${error.message}</p>`;
-    } finally {
-      progressBar.style.display = "none";
-    }
-  });
-
-  downloadButton.addEventListener("click", () => {
-    const link = document.createElement("a");
-    link.href = removedImage.src;
-    link.download = "removed_background.png";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  });
-
-  imageInput.addEventListener("change", () => {
-    if (imageInput.files.length) {
-      const imageUrl = URL.createObjectURL(imageInput.files[0]);
-      previewImage.src = imageUrl;
-    } else {
-      previewImage.src = "#";
-    }
-  });
-});
-
-
 const textElement = document.getElementById("typingText");
 const textLines = textElement.innerHTML.split("<br>");
 
-textElement.textContent = ""; // Clear the text content
+textElement.textContent = "";
 
 let lineIndex = 0;
 let letterIndex = 0;
@@ -107,18 +36,17 @@ function typeNextLetter() {
     if (letterIndex < line.length) {
       textElement.innerHTML += line[letterIndex];
       letterIndex++;
-      setTimeout(typeNextLetter, 50); // Adjust the delay as needed
+      setTimeout(typeNextLetter, 50);
     } else {
       lineIndex++;
       letterIndex = 0;
       textElement.innerHTML += "<br>";
-      setTimeout(typeNextLetter, 300); // Add a longer delay between lines
+      setTimeout(typeNextLetter, 300); 
     }
   }
 }
 
 typeNextLetter();
-
 
 
 const observerOptions = {
---
+++ tools/bg_remv.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const imageInput = document.getElementById("imageInput");
+    const removeButton = document.getElementById("removeButton");
+    const previewImage = document.getElementById("previewImage");
+    const progressBar = document.getElementById("progressBar");
+    const progressFill = document.getElementById("progressFill");
+    const resultDiv = document.getElementById("result");
+    const removedImage = document.getElementById("removedImage");
+    const downloadButton = document.getElementById("downloadButton");
+  
+    removeButton.addEventListener("click", async () => {
+      if (!imageInput.files.length) {
+        alert("Please select an image.");
+        return;
+      }
+  
+      const apiKey = "3FYJwQMAdNc1SKoQpHU92iZN"; 
+      const imageFile = imageInput.files[0];
+  
+      const formData = new FormData();
+      formData.append("image_file", imageFile);
+  
+      try {
+        progressBar.style.display = "block";
+        progressFill.style.width = "0";
+  
+        const response = await fetch("https://api.remove.bg/v1.0/removebg", {
+          method: "POST",
+          headers: {
+            "X-Api-Key": apiKey,
+          },
+          body: formData,
+        });
+  
+        if (!response.ok) {
+          throw new Error("API request failed");
+        }
+  
+        const blob = await response.blob();
+        const imageUrl = URL.createObjectURL(blob);
+  
+        removedImage.src = imageUrl;
+        downloadButton.style.display = "block";
+        resultDiv.style.display = "block";
+      } catch (error) {
+        resultDiv.innerHTML = `<p>Error: ${error.message}</p>`;
+      } finally {
+        progressBar.style.display = "none";
+      }
+    });
+  
+    downloadButton.addEventListener("click", () => {
+      const link = document.createElement("a");
+      link.href = removedImage.src;
+      link.download = "removed_background.png";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  
+    imageInput.addEventListener("change", () => {
+      if (imageInput.files.length) {
+        const imageUrl = URL.createObjectURL(imageInput.files[0]);
+        previewImage.src = imageUrl;
+      } else {
+        previewImage.src = "#";
+      }
+    });
+  });
